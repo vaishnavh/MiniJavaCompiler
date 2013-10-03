1cc2dc5e512ee0ea2a9970a07383930482a474b8
vaishnavh <vaishnavh.nagarajan@gmail.com>
2013-10-03 18:53:46 +0530

    First Commit

---
+++ visitor/Dummy.java
@@ -0,0 +1,42 @@
+package visitor;
+
+import syntaxtree.BinOp;
+import syntaxtree.CJumpStmt;
+import syntaxtree.Call;
+import syntaxtree.ErrorStmt;
+import syntaxtree.Exp;
+import syntaxtree.HAllocate;
+import syntaxtree.HLoadStmt;
+import syntaxtree.HStoreStmt;
+import syntaxtree.IntegerLiteral;
+import syntaxtree.JumpStmt;
+import syntaxtree.Label;
+import syntaxtree.MoveStmt;
+import syntaxtree.NoOpStmt;
+import syntaxtree.Operator;
+import syntaxtree.PrintStmt;
+import syntaxtree.Procedure;
+import syntaxtree.Stmt;
+import syntaxtree.StmtExp;
+import syntaxtree.Temp;
+
+public class Dummy extends  GJNoArguDepthFirst<String>{
+	//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	 
+}
